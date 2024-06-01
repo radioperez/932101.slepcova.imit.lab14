fce28b7325ed432f718d32d4ceccec238ef62c76
radioperez <brostikslavik@yandex.com>
2024-06-01 10:05:45 +0700

    Uploaded gui

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ rng = default_rng()
 class Client:
     timeProcess: int = None
     timeInQueue: int = None
+    label: QLabel = None
     def __init__(self):
         self.timeProcess = int(max(rng.exponential(12), 1))
         self.timeInQueue = 0
+        self.label = QLabel(f'Клиент: {self.timeProcess}, в очереди: {self.timeInQueue}')
 
 class Operator:
     name: str = None
@@ -44,7 +46,10 @@ class Operator:
     def process(self):
         if self.currentClient == None: return
         self.currentClient.timeProcess -= 1
+        self.currentClient.label.setText(f'Клиент: {self.currentClient.timeProcess}, у оператора: {self.name}')
+        self.currentClient.label.update()
         if self.currentClient.timeProcess <= 0:
+            self.currentClient.label.deleteLater()
             self.currentClient = None
 
 class Queue:
@@ -58,6 +63,8 @@ class Queue:
     def run(self):
         for pers in self.queue:
             pers.timeInQueue += 1
+            pers.label.setText(f'Клиент: {pers.timeProcess}, в очереди: {pers.timeInQueue}')
+            pers.label.update()
 
         self.time -= 1
         if self.time <= 0:
@@ -69,6 +76,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Симулятор очереди банка')
+        self.setMinimumWidth(900)
 
         panel = QHBoxLayout()
         # start button
@@ -97,23 +105,44 @@ class MainWindow(QMainWindow):
 
     def start(self):
         self.q = Queue()
-        self.display.addRow(QLabel(f'Клиент: {self.q.queue[0].timeProcess}, в очереди: {self.q.queue[0].timeInQueue}'))
-        self.ops: list[Operator] = [Operator('Alex'), Operator('Brunhilda'), Operator('Crissy'), Operator('Dan')]
+        self.display.addRow(self.q.queue[-1].label)
+        self.ops: list[Operator] = [Operator('Александра'), Operator('Бронеслава'), Operator('Валерия'), Operator('Дан')]
         for index, op in enumerate(self.ops):
-            self.area.addWidget(QLabel(f'{op.name}'), 1, index)
+            oplabel = QLabel(f'{op.name}')
+            oplabel.setStyleSheet('font-weight: 600;')
+            self.area.addWidget(oplabel, 0, index)
         
-        self.timer.setInterval(100)
+        self.timer.setInterval(500)
         self.timer.timeout.connect(self.run)
         self.timer.start()
 
     def run(self):
-        pass
+        freeops = [op for op in self.ops if op.currentClient == None]
+        for index, op in enumerate(freeops):
+            if len(self.q.queue) == 0: continue
+            person = self.q.queue.pop()
+            op.accept(person)
+            person.label.setText(f'Клиент: {person.timeProcess}, у оператора: {op.name}')
+            self.display.update()
+        for index, op in enumerate(self.ops):
+            op.process()
+            self.display.update()
+        self.q.run()
+        for person in self.q.queue:
+            self.display.addRow(person.label)
+        
     def stop(self):
         self.timer.stop()
+        for index, op in enumerate(self.ops):
+            stat = QLabel(f'{op.Nofclients} клиентов,\nсреднее время\nожидания: {op.averageTime:.2f}')
+            self.area.addWidget(stat, 1, index)
+        for person in self.q.queue: person.label.deleteLater()
+        self.q.queue = []
 
 
 random.seed()
 app = QApplication(sys.argv)
+app.setStyleSheet("QLabel {font-size: 20px; text-align: center;} QPushButton {font-size: 20px;}")
 main = MainWindow()
 main.show()
 app.exec()
